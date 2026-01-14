bd91bf20bf1a71ca33d784f4d567cbcf9a1ddb77
Sonia Bolivard <biloums@gmail.com>
2026-01-14 11:53:07 +0100

    navbar added

--- src/app/home.component/home.component.ts
+++ src/app/home.component/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { AuthService } from '../login.service/auth.service';
 import { User } from '../models/user';
+import { NavbarComponent } from '../navbar/navbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })

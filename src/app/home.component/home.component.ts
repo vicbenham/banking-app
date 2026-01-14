6434affe23e44bfa7de572221e3494709e28e5dd
Victor Benhamou <vbenhamou98@gmail.com>
2026-01-14 13:22:25 +0100

    modified home for dynamic data

--- src/app/home.component/home.component.ts
+++ src/app/home.component/home.component.ts
@@ -1,9 +1,10 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, signal, computed, effect} from '@angular/core';
 import { AuthService } from '../login.service/auth.service';
 import { User } from '../models/user';
 import {RouterLink} from '@angular/router';
 import {AccountService} from '../account.component/account.service';
 import {toSignal} from '@angular/core/rxjs-interop';
+import { Account } from '../models/account'; // adapte le chemin si besoin
 
 @Component({
   selector: 'app-home',
@@ -19,10 +20,29 @@ export class HomeComponent {
   private accountService = inject(AccountService)
   signalAccounts = toSignal(this.accountService.getAccounts())
 
+  selectedAccountId = signal<string | null>(null);
+  selectedAccount = computed(() => {
+    const id = this.selectedAccountId();
+    if (!id) return undefined;
+    return (this.signalAccounts() ?? []).find((a: Account) => a.id === id);
+  });
+
+  onAccountChange(event: Event) {
+    const id = (event.target as HTMLSelectElement).value;
+    this.selectedAccountId.set(id);
+  }
+
   constructor(
     private auth: AuthService,
   ) {
     this.user.set(this.auth.getUser());
     console.log('USER FROM STORAGE', this.user());
+
+    effect(() => {
+      const accounts = this.signalAccounts() ?? [];
+      if (!this.selectedAccountId() && accounts.length) {
+        this.selectedAccountId.set(accounts[0].id);
+      }
+    });
   }
 }

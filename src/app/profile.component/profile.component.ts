64ffd320ce60dda8df2e0c2c7e647943bf83895c
morgane <morgane.bth10@gmail.com>
2026-01-14 11:54:23 +0100

    removed FirstLetterPipe

--- src/app/profile.component/profile.component.ts
+++ src/app/profile.component/profile.component.ts
@@ -2,13 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { User } from '../models/user';
 import { AuthService } from '../login.service/auth.service';
 import { RouterLink } from '@angular/router';
-import { FirstLetterPipe } from '../pipe/first-letter-pipe';
 import { FirstLetterIcon } from '../first-letter-icon/first-letter-icon';
 
 @Component({
   selector: 'app-profile.component',
   standalone: true,
-  imports: [RouterLink, FirstLetterPipe, FirstLetterIcon],
+  imports: [RouterLink, FirstLetterIcon],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })

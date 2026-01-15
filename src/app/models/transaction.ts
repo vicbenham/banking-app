7d00e290718880bfb00075d9951d7ff3006bc1bf
sana <sanamouamar@gmail.com>
2026-01-15 22:57:44 +0100

    fix transaction detail

--- src/app/models/transaction.ts
+++ src/app/models/transaction.ts
@@ -1,6 +1,9 @@
 export interface Transaction{
   id: string;
   receiverAccountId: string;
+  receiverName: string;
+  type: string;
+  date: Date;
   amount: number;
   status: string;
   description: string;
--- src/app/transaction/transaction.detail/transaction.detail.ts
+++ src/app/transaction/transaction.detail/transaction.detail.ts
@@ -1,26 +1,55 @@
-import {Component, Input} from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
-import {NavbarComponent} from '../../navbar/navbar';
-import {FirstLetterIcon} from '../../first-letter-icon/first-letter-icon';
-import {formatDate, NgClass} from '@angular/common';
-import {Transaction} from '../../models/transaction';
-import {HttpHeaders} from '@angular/common/http';
+import {Component, Input, Output, EventEmitter, ChangeDetectorRef} from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { NavbarComponent } from '../../navbar/navbar';
+import { FirstLetterIcon } from '../../first-letter-icon/first-letter-icon';
+import { Transaction } from '../../models/transaction';
+import {DatePipe, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-transaction-detail',
+  standalone: true,
   imports: [
     NavbarComponent,
     FirstLetterIcon,
     RouterLink,
-    NgClass
+    NgClass,
+    DatePipe,
   ],
   templateUrl: './transaction.detail.html',
   styleUrl: './transaction.detail.css',
 })
 export class TransactionDetail {
-  transaction! : Transaction
-  constructor(private router: Router) {
+
+  transaction!: Transaction;
+  canceled = false;
+  timer: any;
+
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {
     this.transaction = history.state['transaction'];
     console.log('transaction reçue = ', this.transaction);
+    if (!this.transaction.date) {
+      this.transaction.date = new Date();
+    }
+
+
+    // Timer 10 secondes si statut = PENDING
+    if (this.transaction.status.toLowerCase() === 'pending') {
+      this.timer = setTimeout(() => {
+        if (!this.canceled) {
+          this.transaction.status = 'success';
+          this.cdr.detectChanges();
+        }
+      }, 10000);
+    }
+
   }
+
+
+  onCancel() {
+    this.canceled = true;
+    this.transaction.status = 'failed';
+    clearTimeout(this.timer);
+    this.cdr.detectChanges();
+  }
+
 }
